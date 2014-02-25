437122564be0136c2cad5f28f4bbc24e5c9188d3
Barry Lam <blam789@outlook.com>
2014-02-25 17:55:06 -0500

    added views/index.erb and modified server.rb

--- server.rb
+++ server.rb
@@ -1,33 +1,9 @@
 require 'sinatra'
 
-# stating that our static files are in the 'public' folder
-set :public_folder, File.dirname(__FILE__) + '/public'
-
 get '/' do
-  tasks = ['pay bills', 'buy milk', 'learn Ruby' ]
-
-  html = '''
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <title>Basic HTML Page</title>
-      <link rel="stylesheet" href="home.css" />
-    </head>
-
-    <body>
-      <h1>TODO list</h1>
-
-      <ul>
-  '''
-
-
-  tasks.each do |task|
-    html += "<li>#{task}</li>"
-  end
-
-  html += '''
-        </ul>
-      </body>
-    </html>
-    '''
+  @tasks = ['pay bills', 'buy milk', 'learn Ruby' ]
+  erb :index
 end
+
+set :views, File.dirname(__FILE__) + '/views'
+set :public_folder, File.dirname(__FILE__) + '/public'
